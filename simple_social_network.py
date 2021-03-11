6d2337976fe81c9e64a4971cc3c706ace4a1c16d
Randall Mentzos <theguy77@umd.edu>
2021-03-11 13:07:46 -0500

    added a skeleton of initialize & connect methods

--- simple_social_network.py
+++ simple_social_network.py
@@ -10,4 +10,14 @@ class Person:
             who know this person.
     """
 
+    def __init__(self, name):
+        """ Initialize a new Person object """
+        self.name = name
+        self.connections = set()
     
+    def connect(self, person2):
+        """ Connect with person2 
+
+        Args:
+            person2 (Person): the toher person to connect to.
+        """
